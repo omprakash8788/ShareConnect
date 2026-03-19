918f61ea3987aa84870d056ab65c043b5dfbbb5b
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-19 15:11:10 +0530

    setup backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
 import ConnectedToMongoDBDataBase from "./db/databasedconnection";
 dotenv.config();
 
 const app = express();
 const port_server = process.env.PORT;
 console.log(port_server)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
 
 // Databased call
 ConnectedToMongoDBDataBase()
