6ec9f771df604a1e2125f488b9c25132372a03b0
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-21 14:54:43 +0530

    create api for list all users

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,7 +2,8 @@ import userModel from "../models/user.model";
 import dbErrorHandler from "../helpers/dbErrorHandler";
 import { Request, Response, NextFunction } from "express";
 
-const create = async (req: Request, res: Response, next: NextFunction) => {
+
+const createNewUser = async (req: Request, res: Response, next: NextFunction) => {
   const user = new userModel(req.body);
   try {
     await user.save();
@@ -11,28 +12,35 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
     return res.status(400).json({
       message: "Error while signed up!" + error
     })
-
   }
 }
 
-const list = (req, res) => {
+const listAllUsers = async(req:Request, res:Response) => {
+  try {
+    let users = await userModel.find().select('name email updated created')
+    res.json(users)
+    
+  } catch(error) {
+      return res.status(400).json({
+      message: "Error while Fetching users!" + error
+    })
+  }
+}
+
+const userByID = (req:Request, res:Response, next:NextFunction, id:string) => {
 
 }
 
-const userByID = (req, res, next, id) => {
+const read = (req:Request, res:Response) => {
 
 }
 
-const read = (req, res) => {
+const update = (req:Request, res:Response, next:NextFunction) => {
 
 }
 
-const update = (req, res, next) => {
+const remove = (req:Request, res:Response, next:NextFunction) => {
 
 }
 
-const remove = (req, res, next) => {
-
-}
-
-export default { create, userByID, read, list, remove, update }
+export default { createNewUser, userByID, read, listAllUsers, remove, update }
