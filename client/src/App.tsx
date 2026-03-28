e825fba430f970a1c6fe8ed761f86ff5aa450d37
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-28 22:14:38 +0530

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <div className="bg-red-300">
+      <Navbar/>
        <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
