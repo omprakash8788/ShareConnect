cdf04f709482a51609508a06035b741663302500
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-31 21:07:21 +0530

    home page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,8 +1,12 @@
-
+import video from "../assets/medium.mp4";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="w-[60%] items-center mx-auto flex justify-between">
+      <video autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
