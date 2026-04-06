a17a0580dad4d9e7f95fd89ef38a9b5e7070d5c2
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-06 20:31:12 +0530

    read api

--- client/src/users/api-user.ts
+++ client/src/users/api-user.ts
@@ -2,6 +2,14 @@ import type { User } from "../types/user";
 
 const API = import.meta.env.VITE_API_URL;
 
+type Params = {
+    userId: string;
+};
+
+type Credentials = {
+    t: string; 
+};
+
 const create = async (user: User) => {
     try {
         const response = await fetch(`${API}/api/users/`, {
@@ -30,5 +38,24 @@ const list = async (signal: AbortSignal) => {
     }
 }
 
+const read = async (params: Params, credentials: Credentials, signal: AbortSignal) => {
+    try {
+        const response = await fetch('/api/users/' + params.userId, {
+            method: 'GET',
+            signal: signal,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            }
+        })
+        return await response.json()
+    } catch (err) {
+        console.log(err)
+    }
+}
 
-export { create, list }
+
+export { create, list, read }
+
+
