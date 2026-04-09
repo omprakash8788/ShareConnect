9ad4ab4678d649257750d032f41d071acc1035b7
omprakash kumar <112754995+omprakash8788@users.noreply.github.com>
2026-04-09 22:30:32 +0530

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { BiLogoFacebookSquare } from "react-icons/bi";
 const Navbar = () => {
+  
   return (
     <div className="bg-gray-400 w-[60%] items-center mx-auto flex justify-between">
         <div className="w-10 h-10">
