7026d56d911db0c4183c6d56d95299654169ae8b
omprakash kumar <112754995+omprakash8788@users.noreply.github.com>
2026-04-10 20:08:50 +0530

    Update databasedconnection.ts

--- server/src/db/databasedconnection.ts
+++ server/src/db/databasedconnection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectedToMongoDBDataBase =async ()=>{
     mongoose.connection.on("connected",()=>{
-      console.log("Data based connected")
+      console.log("Databased connected ")
     })
     await mongoose.connect(`${process.env.MONGODB_URI}`)
 
